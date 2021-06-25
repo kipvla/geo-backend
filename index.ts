a2e2d543d75d64124cf889d17a6e493c5d25ede1
Santiago Vásquez Yacamán <santiagoyacaman@uninorte.edu.co>
2021-06-25 16:54:01 -0500

    feat: Basic server setup

---
+++ index.ts
@@ -0,0 +1,17 @@
+const cors = require('cors');
+const express = require('express');
+const connectDB = require('./db/db');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/user', require('./routes/user'));
+app.use('/auth', require('./routes/auth'));
+
+connectDB();
+app.listen(PORT, () => console.log('Server running!'));
