b80a0536661784cea649b4889ef1d0978cf4fa96
Santiago Vásquez Yacamán <santiagoyacaman@uninorte.edu.co>
2021-06-26 10:45:18 -0500

    feat: fixed aws issue

--- controllers/places.ts
+++ controllers/places.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from 'express';
-import AWS from 'aws-sdk';
+import AWS from 'aws-sdk'
 import shortid from 'shortid';
 import moment from 'moment';
-
 import Place from '../models/place';
+import dotenv from 'dotenv'
+dotenv.config();
 
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_S3_ID,
@@ -29,7 +30,7 @@ const uploadImage = async (file: any, type: any) => {
   };
 
   try {
-    /* @ts-ignore */
+     /* @ts-ignore */
     const { Location } = await s3.upload(params).promise();
     let location = Location || '';
     return location;
